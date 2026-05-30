7afabed35672ebce292ffc6562fe4b25cae4b7d7
Mahdi Yazdani <mahdiyazdani@msn.com>
2026-05-30 13:32:14 +0300

    feat: initialize rest api controller

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -66,7 +66,43 @@ if ( ! class_exists( 'ExitSure_Sync_Plugin' ) ) {
 		 * @return void
 		 */
 		public function init() {
+			$this->load_dependencies();
+			$this->init_rest_api();
+				
 			add_action( 'init', array( $this, 'load_textdomain' ) );
+		}
+
+		/**
+		 * Loads plugin dependencies.
+		 *
+		 * @return void
+		 */
+		private function load_dependencies() {
+			$files = array(
+				EXITSURE_SYNC_PATH . 'includes/class-rest-controller.php',
+			);
+
+			foreach ( $files as $file ) {
+				if ( ! file_exists( $file ) ) {
+					continue;
+				}
+
+				require_once $file;
+			}
+		}
+
+		/**
+		 * Initializes REST API controllers.
+		 *
+		 * @return void
+		 */
+		private function init_rest_api() {
+			if ( ! class_exists( 'ExitSure_Sync_REST_Controller' ) ) {
+				return;
+			}
+
+			$rest_controller = new ExitSure_Sync_REST_Controller();
+			$rest_controller->init();
 		}
 
 		/**
